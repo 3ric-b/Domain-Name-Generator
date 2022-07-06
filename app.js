d7643afd73cb156c15eaaec11d0e42f326674123
b5720 <baez5720@outlook.com>
2022-07-06 19:18:49 +0000

    se agrega app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+let createDomain = () => {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["dog", "fish"];
+    let dom = [".com", ".es", ".org"];
+  
+  
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let h = 0; h < dom.length; h++) {
+            console.log(pronoun[i] + adj[j] + noun[k] + dom[h]);
+          }
+        }
+      }
+    }
+  };
+  createDomain()
